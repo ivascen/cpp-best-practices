09d780f2f63112084078770d0a715a5376deef8f
ivan valeriani <ivan.valeriani@gmail.com>
2019-02-18 18:54:44 +0000

    init

---
+++ cpp-best-practices/cpp-best-practices.cpp
@@ -0,0 +1,12 @@
+﻿// cpp-best-practices.cpp : Defines the entry point for the application.
+//
+
+#include "cpp-best-practices.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
